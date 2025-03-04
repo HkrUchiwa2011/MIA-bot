5d89ea9134954def55f917c176d857d5af29aae2
HkrUchiwa2011 <fadilbouko780@gmail.com>
2025-03-04 21:11:55 +0100

    Update cmds

---
+++ scripts/cmds/bal.js
@@ -0,0 +1,31 @@
+const fs = require("fs");
+const balanceFile = "balance.json";
+
+module.exports = {
+  config: {
+    name: "bal",
+    version: "1.0",
+    author: "L'Uchiha Perdu",
+    role: 0,
+    shortDescription: "Voir son solde",
+    longDescription: "Affiche uniquement l'argent en main de l'utilisateur.",
+    category: "💰 Économie",
+    guide: "{p}bal"
+  },
+
+  onStart: async function ({ message, event }) {
+    const userID = event.senderID;
+    let bankData = {};
+
+    if (fs.existsSync(balanceFile)) {
+      bankData = JSON.parse(fs.readFileSync(balanceFile));
+    }
+
+    if (!bankData[userID]) {
+      bankData[userID] = { cash: 0, bank: 0 };
+      fs.writeFileSync(balanceFile, JSON.stringify(bankData, null, 2));
+    }
+
+    message.reply(`💰 **Solde actuel :**\n👜 En main : ${bankData[userID].cash} 💸`);
+  }
+};
